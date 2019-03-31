0f1bb0dc9f80ea890030ddec0692aa4d0bc502ae
lkaae2018 <lak@djhhadsten.dk>
2019-03-31 13:44:49 +0200

    Ny version af random

--- inputlak.py
+++ inputlak.py
@@ -1,6 +1,6 @@
 name = input("Enter a name: ")
 print(name)
-x = int(input("Tell X"))
+x = int(input("Tell X: "))
 if x == 4:
     print('You guessed correctly!')
     print('End of program.')
--- shuffle.py
+++ shuffle.py
@@ -1,5 +1,5 @@
 import random
 
-listen=[1,2,3,4,5,6,7,8]
+listen=[1,2,3,4,5,6,7,8,9]
 print (str(random.shuffle(listen)))
 
